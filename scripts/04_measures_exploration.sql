860f807f38fb1538b4b382d95df23675a214d878
filip-Nowak190 <filip.mowak.981@gmail.com>
2026-09-14 19:46:07 +0200

    Update 04_measures_exploration.sql

--- scripts/04_measures_exploration.sql
+++ scripts/04_measures_exploration.sql
@@ -1,18 +1,18 @@
 -- Find the Total Sales
 Select sum(Sales_Amount) as Total_SALES  from gold.fact_sales 
 -- Find how many items are sold
-Select SUM(sales_Quantity) as Total_sales_quantity from gold.fact_sales 
+Select SUM(Quantity) as Total_sales_quantity from gold.fact_sales 
 -- Find the average selling price
 Select AVG(Price) as Avg_Price  From gold.fact_sales
 -- Find the Total number of Orders
 Select COUNT(Order_Number) AS total_orders from gold.fact_sales
 Select COUNT(DISTINCT(Order_Number)) AS total_orders from gold.fact_sales
 -- Find the total number of products
-Select COUNT(Product_Name) AS total_products from dbo.gold_dim_product
+Select COUNT(Product_Name) AS total_products from gold.dim_products
 -- Find the total number of customers
-Select COUNT(Customer_number_key) AS Total_Customers FROM gold.dim_customer
+Select COUNT(customer_key) AS Total_Customers FROM gold.dim_customers
 -- Find the total number of customers that has placed an order
-SELECT COUNT(DISTINCT Customer_number_key) AS total_customers FROM gold.fact_sales;
+SELECT COUNT(DISTINCT customer_key) AS total_customers FROM gold.fact_sales;
 
 
 Select 'Total Sales' as measure_name, sum(Sales_Amount) as measure_value  from gold.fact_sales 
@@ -23,6 +23,6 @@ Select 'AVG Price' as measure_name, AVG(Price) as measure_value  from gold.fact_
 UNION ALL
 Select 'Total Orders' as measure_name, COUNT(DISTINCT(Order_Number)) as measure_value  from gold.fact_sales 
 UNION ALL
-Select 'Total Products' as measure_name, COUNT(Product_Name) as measure_value  from dbo.gold_dim_product
+Select 'Total Products' as measure_name, COUNT(Product_Name) as measure_value  from gold.dim_products
 UNION ALL
-Select 'Total Customers' as measure_name, COUNT(DISTINCT Customer_number_key)as measure_value  from gold.dim_customer
+Select 'Total Customers' as measure_name, COUNT(DISTINCT customer_key)as measure_value  from gold.dim_customers
