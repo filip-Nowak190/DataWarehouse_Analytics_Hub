58489bf4a9277e2510a55c327bbbd9c93eb6ea85
filip-Nowak190 <filip.mowak.981@gmail.com>
2026-09-14 21:11:08 +0200

    Update 01_database_exploration.sql

--- scripts/01_database_exploration.sql
+++ scripts/01_database_exploration.sql
@@ -1,3 +1,18 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
+
+Table Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/
+
+
 -- Retrieve a list of all tables in the database
 SELECT 
     TABLE_CATALOG, 
